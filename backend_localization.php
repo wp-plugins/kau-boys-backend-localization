734d305e72ddca6c745c90418454fcf0cb0f6711
Kau-Boy <Kau-Boy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-06 17:38:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kau-boys-backend-localization/trunk@152540 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- backend_localization.php
+++ backend_localization.php
@@ -11,6 +11,76 @@ Author URI: http://kau-boys.ramarka.de/blog
 
 define('BACKEND_LOCALIZATION_URL', WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)));
 
+$wp_locale_all = array(
+	'af' => __('Afrikaans'),
+	'ar' => __('Arabic'),
+	'az' => __('Azerbaijani'),
+	'bg_BG' => __('Bulgarian'),
+	'bn_BD' => __('Bengali'),
+	'bs_BS' => __('Bosnian'),
+	'ca' => __('Catalan'),
+	'ckb' => __('Kurdish'),
+	'cs_CZ' => __('Czech'),
+	'cy' => __('Cymraeg'),
+	'da_DK' => __('Danish'),
+	'de_DE' => __('German'),
+	'el' => __('Greek'),
+	'en_US' => __('English'),
+	'eo' => __('Esperanto'),
+	'es_ES' => __('Spanish'),
+	'et' => __('Estonian'),
+	'eu' => __('Basque'),
+	'fa_IR' => __('Persian'),
+	'fi' => __('Finnish'),
+	'fo' => __('Faroese'),
+	'fr_BE' => __('fr_BE'),
+	'fr_FR' => __('French'),
+	'gl_ES' => __('Galician '),
+	'he_IL' => __('Hebrew'),
+	'hi_IN' => __('Hindi'),
+	'hr' => __('Croatian'),
+	'hu_HU' => __('Hungarian'),
+	'id_ID' => __('Indonesian'),
+	'it_IT' => __('Italian'),
+	'ja' => __('Japanese'),
+	'ka_GE' => __('Georgian'),
+	'ko_KR' => __('Korean'),
+	'ky_KY' => __('Kyrgyz'),
+	'lv' => __('Latvian'),
+	'mk_MK' => __('Macedonian'),
+	'ml_IN' => __('Malayalam'),
+	'ms_MY' => __('Malay'),
+	'my_MM' => __('Burmese (Myanmar)'),
+	'nb_NO' => __('Norwegian (Bokmål)'),
+	'nl' => __('Dutch'),
+	'nn_NO' => __('Norwegian (Nynorsk)'),
+	'pl_PL' => __('Polish'),
+	'pt_BR' => __('Portuguese (Brazil)'),
+	'pt_PT' => __('Portuguese'),
+	'ro' => __('Romanian'),
+	'ru_RU' => __('Russian'),
+	'ru_UA' => __('Russian (Ukraine)'),
+	'sd_PK' => __('Sindhi'),
+	'si_LK' => __('Sinhalese'),
+	'sk' => __('Slovak'),
+	'sl_SI' => __('Slovenian'),
+	'sq' => __('Albanian'),
+	'sr_RS' => __('Serbian'),
+	'su_ID' => __('Sundanese'),
+	'sv_SE' => __('Swedish'),
+	'sw' => __('Swahili'),
+	'ta_IN' => __('Tamil'),
+	'th' => __('Thai'),
+	'tr' => __('Turkish'),
+	'ug_CN' => __('Uighur'),
+	'uk' => __('Ukrainian'),
+	'ur' => __('Urdu'),
+	'uz_UZ' => __('Uzbek'),
+	'vi' => __('Vietnamese'),
+	'zh_CN' => __('Chinese'),
+	'zh_HK' => __('Chinese (Hong Kong)')
+);
+
 function init_backend_localization() {
 	load_plugin_textdomain('backend-localization', false, dirname(plugin_basename(__FILE__)));
 }
@@ -31,7 +101,9 @@ function backend_localization_filter_plugin_actions($links, $file){
 }
 
 function backend_localization_admin_settings(){
-	if(isset($_POST['save'])){	
+	global $wp_locale_all;
+	
+	if(isset($_POST['kau-boys_backend_localization_language'])){	
 		$settings_saved = backend_localization_save_setting();
 	}
 	$backend_locale = get_option('kau-boys_backend_localization_language');
@@ -56,7 +128,7 @@ function backend_localization_admin_settings(){
 			<input type="radio" value="<?php echo $locale_value ?>" id="kau-boys_backend_localization_language_<?php echo $locale_value ?>" name="kau-boys_backend_localization_language"<?php echo ($backend_locale == $locale_value)? ' checked="checked"' : '' ?> />
 			<label for="kau-boys_backend_localization_language_<?php echo $locale_value ?>" style="width: 200px; display: inline-block;">
 				<img src="<?php echo BACKEND_LOCALIZATION_URL.'flag_icons/'.strtolower(substr($locale_value, (strpos($locale_value, '_') * -1))).'.png' ?>" alt="<?php echo $locale_value ?>" />
-				<?php echo $locale_value ?>
+				<?php echo $wp_locale_all[$locale_value].' ('.$locale_value.')' ?>
 			</label>
 			<br />
 			<?php endforeach ?>
@@ -100,10 +172,29 @@ function backend_localization_save_setting(){
 	return true;
 }
 
+function backend_localization_login_form(){
+	global $wp_locale_all;
+	
+	$backend_locale_array = backend_localization_get_languages();
+?>
+<p>
+	<label>
+		<?php _e('Language', 'backend-localization') ?><br />
+		<select name="kau-boys_backend_localization_language" id="user_email" style="width: 100%;">
+		<?php foreach($backend_locale_array as $locale_value) : ?>
+			<option value="<?php echo $locale_value ?>"><?php echo $wp_locale_all[$locale_value].' ('.$locale_value.')' ?></option>
+		<?php endforeach ?>
+		</select>
+	</label>
+</p>
+<?php
+}
+
 function localize_backend($locale) {
+	// save settings before getting the locale
+	if(isset($_POST['kau-boys_backend_localization_language'])) backend_localization_save_setting();
+	// set langauge if user is in admin area
 	if(defined('WP_ADMIN')) {
-		// save settings before getting the locale
-		if(isset($_POST['save'])) backend_localization_save_setting(); 
 		$locale = get_option('kau-boys_backend_localization_language');
 	}
 	return $locale;
@@ -111,6 +202,8 @@ function localize_backend($locale) {
 
 add_action('init', 'init_backend_localization');
 add_action('admin_menu', 'backend_localization_admin_menu');
+add_action('login_form', 'backend_localization_login_form');
+add_action('login_form_locale', 'localize_backend', 1, 1);
 add_filter('plugin_action_links', 'backend_localization_filter_plugin_actions', 10, 2);
 add_filter('locale', 'localize_backend', 1, 1);
 
